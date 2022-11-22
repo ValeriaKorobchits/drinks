e8a7ef042445c450587178e9fec257ae72326c0f
ValeriaKorobchits <114007094+ValeriaKorobchits@users.noreply.github.com>
2022-11-22 22:30:26 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 (function(){
     const header = document.querySelector('.header');
+    // const image = document.querySelector('.test');
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             header.classList.add('header_active');
+            // image.setAttribute('src', 'https://cdn-icons-png.flaticon.com/512/7728/7728306.png');
         } else {
             header.classList.remove('header_active');
+            // image.setAttribute('src', 'img/logo2.png');
         }
     }
+
+
+
     const slides = document.querySelectorAll('.slide');
     for (const slide of slides) {
         slide.addEventListener('click', () => {
@@ -22,4 +28,7 @@
     }
 }
 ())
+
+
+
 
